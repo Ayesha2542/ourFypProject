058ed2547a161848eacb0581491cc34e9b79a91d
ToqeerFatima <125538720+ToqeerFatima@users.noreply.github.com>
2023-07-10 22:28:57 -0700

    2nd try
    2nd try..................

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView } from "react-native";
 const App=()=>{
-return(
-  <SafeAreaView>
-    <Text>Hello World</Text>
-    <Text>Hello World</Text>
-  </SafeAreaView>
-)
+    return(
+        <SafeAreaView>
+            <Text>Hello World</Text>
+        </SafeAreaView>
+    )
 }
 export default App;
