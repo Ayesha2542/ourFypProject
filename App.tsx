2875f1fa2c1a179a2abc23ba24862bf79474313c
ToqeerFatima <125538720+ToqeerFatima@users.noreply.github.com>
2023-07-10 22:48:38 -0700

    dummy
    dummy 3rd try

--- App.tsx
+++ App.tsx
@@ -1,10 +1,45 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
-const App=()=>{
-    return(
-        <SafeAreaView>
-            <Text>Hello World</Text>
-        </SafeAreaView>
-    )
+
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text, SafeAreaView ,Image} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const HomeScreen=()=>{
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
 }
+
+const DetailsScreen=()=> {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Detail Screens</Text>
+    </View>
+  );
+}
+
+
+const Stack = createNativeStackNavigator();
+
+const App=() =>{
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 export default App;
+
+
+
+
